7f375feccf8cd6403a0cfb28134815ce24465ca2
Michael <bouchmi@cronos.be>
2022-06-15 13:51:48 +0200

    hotspot config test

--- utils/captive_portal/captive_portal.py
+++ utils/captive_portal/captive_portal.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     os.system("nmcli con down {}".format(hotspot_conn_name))
     time.sleep(0.5)
     os.system("nmcli con delete {}".format(hotspot_conn_name))
-    os.system("nmcli con add type wifi ifname {} con-name {} autoconnect yes ssid {}".format(hotspot_interface, hotspot_conn_name,hotspot_ssid, hotspot_password))
+    os.system("nmcli con add type wifi ifname {} con-name {} autoconnect yes ssid {} mode ap".format(hotspot_interface, hotspot_conn_name,hotspot_ssid, hotspot_password))
     os.system("nmcli con modify {} 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared".format(hotspot_conn_name))
     os.system("nmcli con modify {} wifi-sec.key-mgmt wpa-psk".format(hotspot_conn_name))
     #os.system("nmcli con modify {} wifi-sec.psk {}".format(hotspot_conn_name, hotspot_password))
