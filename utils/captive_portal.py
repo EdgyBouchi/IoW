c4fde688a4da0fc80436d52c7568383950eb8323
nickdestrycker <nick@edgise.com>
2022-06-17 18:21:41 +0200

    test

--- utils/captive_portal.py
+++ utils/captive_portal.py
@@ -40,10 +40,10 @@ def index():
         time.sleep(5)
         
         try:
-            subprocess.check_output(f"sudo nmcli dev wifi connect iphone password meloentje")
+            subprocess.check_output("sudo nmcli dev wifi connect iphone password meloentje")
             print('rebooting ...')
-            os.system("mv /etc/rc.local /etc/captive_portal")
-            os.system("mv /etc/main_iow_script /etc/rc.local")
+            #os.system("mv /etc/rc.local /etc/captive_portal")
+            #os.system("mv /etc/main_iow_script /etc/rc.local")
             os.system("sudo nmcli connection delete {}".format(hotspot_conn_name))
             #add check if possible to connect to network and if internet access is possible
             #sleep necessairy for internet to be available
