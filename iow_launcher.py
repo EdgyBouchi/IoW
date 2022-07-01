cb9e9639b117870eca38d27e1fa7cc05e64a8fb2
Michael <bouchmi@cronos.be>
2022-07-01 22:34:14 +0200

    startup script test

--- iow_launcher.py
+++ iow_launcher.py
@@ -8,15 +8,15 @@ if __name__ == "__main__":
     captive_portal_script_path = '/home/pi/Documents/IoW/utils/captive_portal.py'
     main_script_path = '/home/pi/Documents/IoW/main.py'
     if not os.path.exists(json_path):
-        captive_portal_script = subprocess.Popen("sudo python3 {}".format(captive_portal_script_path))
+        captive_portal_script = subprocess.Popen("sudo python3 {}".format(captive_portal_script_path), shell=True)
         if captive_portal_script.wait() != 0:
             print("captive portal failed")
         else:
             print("starting main script")
-            main_script = subprocess.Popen("echo {} | sudo python3 {}".format("ucllstudent", main_script_path))
+            main_script = subprocess.Popen("echo {} | sudo python3 {}".format("ucllstudent", main_script_path),shell=True)
     else:
         # json with wifi data already exists
         print("wifi config json found")
         print("starting main script")
-        main_script = subprocess.Popen("echo {} | sudo python3 {}".format("ucllstudent", main_script_path))
+        main_script = subprocess.Popen("echo {} | sudo python3 {}".format("ucllstudent", main_script_path), shell=True)
 
