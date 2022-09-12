4320ba616f93f05db4d5c954e13f883fb4da700d
RobrechtUlenaersPXL <72489597+RobrechtUlenaersPXL@users.noreply.github.com>
2022-09-12 13:49:55 +0200

    added iow launcher from test branch

---
+++ iow_launcher.py
@@ -0,0 +1,23 @@
+import os.path
+import subprocess
+
+if __name__ == "__main__":
+    print(os.getcwd())
+    os.chdir("/home/pi/Documents/IoW")
+    json_path = '/home/pi/Documents/IoW/utils/captive_portal/user_register.json'
+    captive_portal_script_path = '/home/pi/Documents/IoW/utils/captive_portal.py'
+    main_script_path = '/home/pi/Documents/IoW/main.py'
+    while not os.path.exists(json_path):
+        captive_portal_script = subprocess.Popen("sudo python3 {}".format(captive_portal_script_path), shell=True)
+        if captive_portal_script.wait() != 0:
+            print("captive portal failed")
+            output, error = captive_portal_script.communicate()
+            print(output)
+            print(error)
+            continue
+
+    # json with wifi data already exists
+    print("wifi config json found")
+    print("starting main script")
+    main_script = subprocess.Popen("echo {} | sudo python3 {}".format("ucllstudent", main_script_path), shell=True)
+
