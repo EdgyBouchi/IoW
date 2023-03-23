9cd623913e3bf879b80ea86c8a28f54f82544230
Jochen-TPO <97177768+Jochen-TPO@users.noreply.github.com>
2023-03-23 13:13:48 +0100

    Removed "force reboot" from crontab_pull script

--- crontab_pull.sh
+++ crontab_pull.sh
@@ -12,12 +12,6 @@ sleep 60
 
 bash ./enlargeSwap.sh
 
-FILE=./forceReboot1
-if [ ! -f "$FILE" ]; then
-  touch $FILE
-  sudo reboot
-fi
-
 tmux send-keys -t iow C-c
 sleep 5
 tmux send-keys -t iow "sudo python3 Documents/IoW/iow_launcher.py" Enter
